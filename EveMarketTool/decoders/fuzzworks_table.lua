6f67e2009a9ffd70b6216b76ab1f5b14191a6d8c
Memcallen <memcallen5@gmail.com>
2018-02-02 12:52:05 -0500

    Added the ability to set cell colors via the table generator

--- EveMarketTool/decoders/fuzzworks_table.lua
+++ EveMarketTool/decoders/fuzzworks_table.lua
@@ -1,3 +1,10 @@
+function isnan(num)
+	return tostring(num) == tostring(0/0)
+end
+
+function isinfinite(num)
+	return tostring(num) == tostring(1/0)
+end
 
 function getColumnTypes()
 	--Name, Buy, Sell, %Margin Volume
@@ -9,13 +16,26 @@ function getColumnNames()
 	return {"Name", "Buy Price", "Sell Price", "% Margin", "Volume"}
 end
 
+function translateTableCol(buy, sell, opts)
+	
+	r = translateTable(buy, sell)
+	opts[2]:Set(r[1])
+	if isnan(r[4]) or isinfinite(r[4]) then
+		opts[1]:Set("red")
+	else
+		opts[1]:Set("none")
+	end
+		
+	return r
+end
+
 function translateTable(buy, sell)
 	
 	return {
 		getItemName(buy["type"]),
 		buy.topFive,
 		sell.topFive,
-		(buy.topFive - sell.topFive) / buy.topFive,
+		(sell.topFive - buy.topFive) / buy.topFive,
 		buy.volume
 	}
 	
