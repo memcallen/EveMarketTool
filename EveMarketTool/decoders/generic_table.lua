79b70efd7d1daa680033489b17b63068efe839a8
Memcallen <memcallen5@gmail.com>
2018-02-16 12:09:45 -0500

    Not coloring, but accesses filters correctly

--- EveMarketTool/decoders/generic_table.lua
+++ EveMarketTool/decoders/generic_table.lua
@@ -6,6 +6,14 @@ function isinfinite(num)
 	return tostring(num) == tostring(1/0)
 end
 
+function default(one, def)
+	if one == nil then
+		return def
+	else
+		return one
+	end
+end
+
 function getColumnTypes()
 	--Name, Buy, Sell, %Margin Volume
 	return {"java.lang.String", "java.lang.Double", "java.lang.Double", "java.lang.Double", "java.lang.Integer"}
@@ -18,9 +26,17 @@ end
 
 function translateTableCol(buy, sell, props)
 	
+	maxprice = tonumber(filter:get("Maximum_Price"))
+	minmarg = tonumber(filter:get("Minimum_Margin"))
+	outside_color = default(filter:get("Invalid_Color"), "red")
+	
 	r = translateTable(buy, sell)
 	props:put("name", r[1])
-	if isnan(r[4]) or isinfinite(r[4]) then
+	if not(minmarg == nil) and r[4] < minmarg then
+		props:put("color", outside_color)
+	elseif not(maxprice == nil) and r[2] > maxprice then
+		props:put("color", outside_color)
+	elseif isnan(r[4]) or isinfinite(r[4]) then
 		props:put("color", "red")
 	else
 		props:put("color", "none")
@@ -35,7 +51,7 @@ function translateTable(buy, sell)
 		getItemName(buy["type"]),
 		buy.topFive,
 		sell.topFive,
-		(sell.topFive - buy.topFive) / buy.topFive,
+		(sell.topFive - buy.topFive) / buy.topFive, --margin
 		buy.volume
 	}
 	
